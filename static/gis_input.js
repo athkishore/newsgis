bc1084f74786dbc50350db8a969bcd398a315543
Kishore A <kishore.asok@gmail.com>
2016-01-14 10:36:11 -0500

    Adding points using click event trigger working: ready for more development

--- static/gis_input.js
+++ static/gis_input.js
@@ -1,26 +1,67 @@
-OpenLayers.ProxyHost = "http://localhost:5000/proxy?url=";
-var map, drawPoint; //, infoControls, highlightLayer;
+var map, pointLayer; 
 var bounds = new OpenLayers.Bounds(
 		8434465,1214769,
 		8585072,1131719
 );
 
+OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
+    defaultHandlerOptions: {
+        'single': true,
+        'double': false,
+        'pixelTolerance': 0,
+        'stopSingle': false,
+        'stopDouble': false
+    },
+    
+    initialize: function(options) {
+        this.handlerOptions = OpenLayers.Util.extend(
+            {}, this.defaultHandlerOptions
+        );
+        OpenLayers.Control.prototype.initialize.apply(
+            this, arguments
+        );
+        this.handler = new OpenLayers.Handler.Click(
+            this, {
+                'click': this.trigger
+            }, this.handlerOptions
+        );
+    },
+    
+    trigger: function(e) {
+        var proj = new OpenLayers.Projection("EPSG:4326");
+        var lonlat_mapproj = map.getLonLatFromPixel(e.xy);
+        var lonlat = new OpenLayers.LonLat(lonlat_mapproj.lon,
+            lonlat_mapproj.lat
+        );
+        lonlat.transform(map.getProjectionObject(), proj);
+        var point = new OpenLayers.Geometry.Point(
+            lonlat_mapproj.lon, lonlat_mapproj.lat
+        );
+        var pointFeature = new OpenLayers.Feature.Vector(
+            point, null, null
+        );
+        pointLayer.addFeatures([pointFeature]);
+        var text = '<p>New point added at '+lonlat.lon+
+            'E, '+lonlat.lat+'N</p>'
+        var response = document.getElementById('responseText').innerHTML;
+        document.getElementById('responseText').innerHTML = response+text;
+    }
+});
+
 function load() {
   map = new OpenLayers.Map('map', {projection: new
-  OpenLayers.Projection("EPSG:900913")});
+      OpenLayers.Projection("EPSG:900913")});
 
   var osm = new OpenLayers.Layer.OSM("OpenStreetMap");
 
-  var pointLayer = new OpenLayers.Layer.Vector("Point Layer");
+  pointLayer = new OpenLayers.Layer.Vector("Point Layer");
         	
   map.addLayers([osm, pointLayer]);
   map.zoomToExtent(bounds);
   map.addControl(new OpenLayers.Control.LayerSwitcher());
-  map.addControl(new OpenLayers.Control.MousePosition());
         
-  drawPoint = new OpenLayers.Control.DrawFeature(pointLayer, 
-                          OpenLayers.Handler.Point)
-
-  map.addControl(drawPoint);          
-  drawPoint.activate();
+  var click = new OpenLayers.Control.Click();
+  map.addControl(click);
+  click.activate();
 }
+
